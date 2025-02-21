88fd1a17365a0cc253da6513e55bad1fcf73f244
Nate Z <ntzeitlin@gmail.com>
2025-02-21 12:31:29 -0600

    users can now post new ships to the database

--- DIAGRAM.md
+++ DIAGRAM.md
@@ -3,16 +3,28 @@ sequenceDiagram
     title Shipping Ships API
 
     participant Client
-    participant Python
-    participant JSONServer
+    participant json-server.py
+    participant JSONServer 
     participant ship_view.py
     participant Database
-    Client->>Python:GET request to "/ships"
-    Python->>JSONServer:Run do_GET() method
+    Client->>json-server.py:GET request to "/ships"
+    json-server.py->>JSONServer:Run do_GET() method
     JSONServer->>ship_view.py:Run retrieve_ship
     ship_view.py->>Database:Run sql query to select ship, if no pk then all ships
     Database->>ship_view.py:Here's all ships
     ship_view.py-->>JSONServer: Here's all ships
     JSONServer-->>Client: Here's all yer ships (in JSON format) and a response code.
+
+    Client->>json-server.py:PUT request to "/ships/1"
+    json-server.py->>JSONServer:Run do_PUT() method
+    JSONServer->>ship_view.py:Run update_ship
+    ship_view.py->>Database:Run UPDATE Ship WHERE id=id
+    Database->>ship_view.py:Here's how many rows were affected
+    ship_view.py-->>JSONServer: True / False depending on rowcount
+    JSONServer-->>Client: Return response to client
+
+
+
+
 
 ```
--- views/ship_view.py
+++ views/ship_view.py
@@ -1,6 +1,26 @@
 import sqlite3
 import json
 
+
+def add_ship(ship_data):
+    with sqlite3.connect("./shipping.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+            """
+            INSERT INTO Ship 
+            (name, hauler_id)
+            VALUES (?, ?)
+                   
+            """,
+            (ship_data["name"], ship_data["hauler_id"]),
+        )
+
+        rows_affected = db_cursor.rowcount
+
+    return True if rows_affected > 0 else False
+
+
 def update_ship(id, ship_data):
     with sqlite3.connect("./shipping.db") as conn:
         db_cursor = conn.cursor()
@@ -13,12 +33,13 @@ def update_ship(id, ship_data):
                     hauler_id = ?
             WHERE id = ?
             """,
-            (ship_data['name'], ship_data['hauler_id'], id)
+            (ship_data["name"], ship_data["hauler_id"], id),
         )
 
         rows_affected = db_cursor.rowcount
 
     return True if rows_affected > 0 else False
+
 
 def delete_ship(pk):
     with sqlite3.connect("./shipping.db") as conn:
@@ -26,9 +47,11 @@ def delete_ship(pk):
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
-        db_cursor.execute("""
+        db_cursor.execute(
+            """
         DELETE FROM Ship WHERE id = ?
-        """, (pk,)
+        """,
+            (pk,),
         )
         number_of_rows_deleted = db_cursor.rowcount
 
@@ -42,17 +65,19 @@ def list_ships():
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
-        db_cursor.execute("""
+        db_cursor.execute(
+            """
         SELECT
             s.id,
             s.name,
             s.hauler_id
         FROM Ship s
-        """)
+        """
+        )
         query_results = db_cursor.fetchall()
 
         # Initialize an empty list and then add each dictionary to it
-        ships=[]
+        ships = []
         for row in query_results:
             ships.append(dict(row))
 
@@ -61,6 +86,7 @@ def list_ships():
 
     return serialized_ships
 
+
 def retrieve_ship(pk):
     # Open a connection to the database
     with sqlite3.connect("./shipping.db") as conn:
@@ -68,14 +94,17 @@ def retrieve_ship(pk):
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
-        db_cursor.execute("""
+        db_cursor.execute(
+            """
         SELECT
             s.id,
             s.name,
             s.hauler_id
         FROM Ship s
         WHERE s.id = ?
-        """, (pk,))
+        """,
+            (pk,),
+        )
         query_results = db_cursor.fetchone()
 
         # Serialize Python list to JSON encoded string
