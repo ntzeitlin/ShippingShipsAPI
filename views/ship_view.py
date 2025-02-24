f8986bd90913130e7e95ab0148ca25d50903d1ad
Nate Z <ntzeitlin@gmail.com>
2025-02-24 09:43:22 -0600

    users can now use the _expand query string parameter on ships

--- views/ship_view.py
+++ views/ship_view.py
@@ -64,22 +64,59 @@ def list_ships(url):
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        # Write the SQL query to get the information you want
-        db_cursor.execute(
-            """
-        SELECT
-            s.id,
-            s.name,
-            s.hauler_id
-        FROM Ship s
-        """
-        )
-        query_results = db_cursor.fetchall()
+        if url["query_params"]:
+            if url["query_params"]["_expand"]:
+                print("expanding...")
+                # Write the SQL query to get the information you want
+                db_cursor.execute(
+                    """
+                SELECT
+                    s.id,
+                    s.name,
+                    s.hauler_id,
+                    h.id haulerId,
+                    h.name haulerName,
+                    h.dock_id
+                FROM Ship s
+                JOIN Hauler h
+                    ON h.id = s.hauler_id
+                """
+                )
+                query_results = db_cursor.fetchall()
 
-        # Initialize an empty list and then add each dictionary to it
-        ships = []
-        for row in query_results:
-            ships.append(dict(row))
+                # Initialize an empty list and then add each dictionary to it
+                ships = []
+                for row in query_results:
+                    hauler = {
+                        "id": row["haulerId"],
+                        "name": row["haulerName"],
+                        "dock_id": row["dock_id"],
+                    }
+                    ship = {
+                        "id": row["id"],
+                        "name": row["name"],
+                        "hauler_id": row["hauler_id"],
+                        "hauler": hauler,
+                    }
+                    ships.append(ship)
+        else:
+
+            # Write the SQL query to get the information you want
+            db_cursor.execute(
+                """
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id
+            FROM Ship s
+            """
+            )
+            query_results = db_cursor.fetchall()
+
+            # Initialize an empty list and then add each dictionary to it
+            ships = []
+            for row in query_results:
+                ships.append(dict(row))
 
         # Serialize Python list to JSON encoded string
         serialized_ships = json.dumps(ships)
